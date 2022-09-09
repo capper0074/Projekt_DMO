d0ea10e1fcb0b8eb44cf08f6fd3c550bfe014eea
Casper <capper2704@hotmail.dk>
2022-09-09 08:38:24 +0200

    More Fixes

--- Domain/Program.cs
+++ Domain/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+
+    public static void Main(String[] args)
+    {
+
+        Console.WriteLine("Test");
+        Console.WriteLine("test");
+
+
+    }
+
+}
--- Visits.cs
+++ Visits.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 public class Class1
 {
 	public Class1()
 	{
+
+
+
 	}
 }
