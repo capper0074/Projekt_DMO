feb5db58dd6518a6507c883cdfbd2aa34a11117d
Mathiaslk3 <mathiaslk3@outlook.dk>
2022-09-13 14:06:30 +0200

    Implamitering af primitiv Menu V0.1

--- Domain/Program.cs
+++ Domain/Program.cs
@@ -2,11 +2,40 @@
 {
 
     public static void Main(String[] args)
-    {
+    {      
+        
+        Console.WriteLine("Hej og velkommen");
+        Console.WriteLine("Vælg en af de nederstående muligheder \n");
 
-        Console.WriteLine("Test");
-        Console.WriteLine("test");
+        //Menu options
+        Console.WriteLine("Mulighed 1");
+        Console.WriteLine("Mulighed 2");
+        Console.WriteLine("Mulighed 3");
+        Console.WriteLine("Mulighed 4");
 
+        Console.WriteLine("Hvad vil du vælge: ");
+
+        int res = Convert.ToInt32(Console.ReadLine());
+
+        switch (res)
+	    {
+            case 1:
+               Console.WriteLine("Hej, Mulighed 1");
+                break;
+            case 2:
+                Console.WriteLine("Hej, Mulighed 2");
+                break;
+            case 3:
+                Console.WriteLine("Hej, Mulighed 3");
+                break;
+            case 4:
+                Console.WriteLine("Hej, Mulighed 4");
+                break;
+		    default:
+                Console.WriteLine("Fejl, du inputtede: " + res);
+                break;
+	    }
+        Console.ReadLine();
 
     }
 
